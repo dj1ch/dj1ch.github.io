793013425e4b8525c1559ed5e4afc4140f5e46d8
dj1ch <tg.1ch0712@gmail.com>
2024-11-27 12:27:30 -0800

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/server";
+import vercel from '@astrojs/vercel/serverless';
 // import node from '@astrojs/node';
 
 export default defineConfig({
